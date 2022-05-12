db3f5c79412480f5504685f82d4af7897dafb290
Darya Kuliashova <kuldiand@gmail.com>
2022-05-12 13:51:01 +0200

    Add Constants

---
+++ UIKitListModule/UIKitListModule/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  UIKitListModule
+//
+//  Created by Darya Kuliashova on 12/05/2022.
+//
+
+struct Constants {
+    static let deleteRowAccessibilityLabel = "Delete Row"
+    static let addRowAccessibilityLabel = "Add Row"
+    static let navbarTitle = "Employees"
+    static let employeeCellIdentifier = "employeeCell"
+}
